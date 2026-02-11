2d761a81ba361fcb9d124c859f3119d988ab2d60
Jitendra <jitendraphone7505@gmail.com>
2026-02-11 18:45:16 +0530

    commit done

--- src/App.jsx
+++ src/App.jsx
@@ -37,34 +37,6 @@ function App() {
         className="svg flex items-center justify-center fixed top-0 left-0 z-10 w-full h-screen overflow-hidden bg-black
       "
       >
-        {/* <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
-          <defs>
-            <mask id="viMask">
-              <rect width="100%" height="100%" fill="black" />
-              <g className="vi-mask-group">
-                <text
-                  x="50%"
-                  y="50%"
-                  fontSize="250"
-                  textAnchor="middle"
-                  fill="white"
-                  dominantBaseline="middle"
-                  fontFamily="Arial Black"
-                >
-                  VI
-                </text>
-              </g>
-            </mask>
-          </defs>
-          <image
-            href="/bg.png"
-            width="100%"
-            height="100%"
-            preserveAspectRatio="xMidYMid slice"
-            mask="url(#viMask)"
-          />
-        </svg> */}
-
         <div className=" h-screen w-full flex items-center justify-center bg-black">
           <h1
             className="VI-text text-[30vw] font-black uppercase"
